aee8e7ca28a505613e816cc2de56de79e14c9be3
Daniel Barcheta Oliveirsa <zru@MacBook-Pro-de-Daniel.local>
2024-03-24 21:59:15 -0300

    PROPS - Passando para o App.jsx os todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Navbar from "./components/Navbar";
-import Tabela from "./components/Tabela";
+import Navbar from "./components/Navbar";import TabelaToDo from "./components/TabelaToDo";
+
+const todos = [
+  {numeroLinha: "1", descricao: "Tomar um cafezinho"},
+  {numeroLinha: "2", descricao: "Ligar o Notebook da Touch"},
+  {numeroLinha: "3", descricao: "Abrir o Discord e dar bom dia pro pessoal"},
+  {numeroLinha: "4", descricao: "Prestar atencao nas aulitas de React"}
+]
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Tabela/>
+      <TabelaToDo todos={todos}/>
 
     </div>
   );
--- src/components/TabelaToDo.js
+++ src/components/TabelaToDo.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ToDoItemLinha from './ToDoItemLinha';
 
-const todos = [
-    {numeroLinha: "1", descricao: "Tomar um cafezinho"},
-    {numeroLinha: "2", descricao: "Ligar o Notebook da Touch"},
-    {numeroLinha: "3", descricao: "Abrir o Discord e dar bom dia pro pessoal"},
-    {numeroLinha: "4", descricao: "Prestar atencao nas aulitas de React"}
-]
 
-function Tabela() {
+function TabelaToDo(props) {
     return (
         <table>
             <thead>
@@ -26,18 +20,17 @@ function Tabela() {
                 /> */}
 
             {   
-            todos.map((toDoRow) => (
+            props.todos.map((toDoRow) => (
                     <ToDoItemLinha 
                         numeroLinha={toDoRow.numeroLinha}
                         descricao={toDoRow.descricao}
                     />
                 ))
             }
-
-
+            
             </tbody>
         </table>
     );
 }
 
-export default Tabela;
+export default TabelaToDo;
