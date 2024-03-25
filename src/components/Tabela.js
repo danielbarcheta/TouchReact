dec92d6518a696cf932366b1486815f1d66ce5a6
Daniel Barcheta Oliveirsa <zru@MacBook-Pro-de-Daniel.local>
2024-03-24 21:41:07 -0300

    PROPS - Iterando sobre um array de objetos para coletar informacoes

--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ToDoItemLinha from './ToDoItemLinha';
 
+const todos = [
+    {numeroLinha: "1", descricao: "Tomar um cafezinho"},
+    {numeroLinha: "2", descricao: "Ligar o Notebook da Touch"},
+    {numeroLinha: "3", descricao: "Abrir o Discord e dar bom dia pro pessoal"},
+    {numeroLinha: "4", descricao: "Prestar atencao nas aulitas de React"}
+]
+
 function Tabela() {
     return (
         <table>
@@ -11,18 +18,23 @@ function Tabela() {
                 </tr>
             </thead>
             <tbody>
+
+                {/* PODERIAMOS FAZER DESSA FORMA, ITERANDO UM POR UM:
                 <ToDoItemLinha 
-                numeroLinha="1" 
-                descricao="Tomar um cafezinho"
-                />
-                <ToDoItemLinha 
-                numeroLinha="2" 
-                descricao="Dar bom dia pro pessoal da touch no discord"
-                />
-                <ToDoItemLinha 
-                numeroLinha="3" 
-                descricao="Fazer ginastica laboral"
-                />
+                numeroLinha={todos[0].numeroLinha}
+                descricao={todos[0].descricao}
+                /> */}
+
+            {   
+            todos.map((toDoRow) => (
+                    <ToDoItemLinha 
+                        numeroLinha={toDoRow.numeroLinha}
+                        descricao={toDoRow.descricao}
+                    />
+                ))
+            }
+
+
             </tbody>
         </table>
     );
